3b193c5d829ae513dda3fc0fa59c5bc258fe470b
Klay <hix_x@hotmail.com>
2023-07-28 14:10:04 -0300

    -

--- mysql_conector.py
+++ mysql_conector.py
@@ -1,1 +1,23 @@
 import mysql.connector
+
+#CRUD em Python - Python e MySQL - youtube.com
+
+conexao = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '1234',
+    database = 'bdyoutube',
+)
+cursor =  conexao.cursor()
+
+#create
+
+comando = 'INSERT INTO'
+cursor.execute(comando)
+conexao.commit()
+
+resultado = cursor.fetchall()
+
+
+cursor.close()
+conexao.close()
