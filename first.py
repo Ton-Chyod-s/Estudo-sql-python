e57340419dd68beb09f0fd60f71e4e8bc2c806ec
Klay <hix_x@hotmail.com>
2023-10-27 16:24:29 -0400

    doc: att

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
+from kivymd.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 
 
@@ -11,12 +12,15 @@ Screen:
             title: 'Myapp'
             left_action_items:[['menu', lambda x: x]]
             right_action_items:[['dots-vertical', lambda x: x]]
-        FormLogin:
+        TelaLogin:
 
         
 <senhacard>:
-    
-<FormLogin@FloatLayout>:
+    TextField:
+        text: 'senha antiga'
+        on_text: root.my_callback_function(args)
+
+<TelaLogin>:
     MDIconButton:
         pos_hint: {'center_x': .5, 'center_y': .8}
         icon: 'language-python'
@@ -46,16 +50,17 @@ Screen:
         text: 'Clique aqui!'
         halign: 'center'
         pos_hint: {'center_x': .5, 'center_y': .2}
-        on_release: app.abrir_card()
+        on_release: root.abrir_card()
 """
 class SenhaCard(MDCard):
     ...
 
+class TelaLogin(FloatLayout):
+    def abrir_card(self):
+        self.add_widget(SenhaCard)
+
 
 class MainApp(MDApp):
-    def abrir_card(self):
-        print('abrindo o card')
-
     def build(self):
         self.theme_cls.theme_style = "Dark"
         return Builder.load_string(KV)
