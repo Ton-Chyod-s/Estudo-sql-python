5df85b24e8cded64aee7e61c71cdab741a84d738
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-14 09:29:28 -0400

    doc: att

--- estudo kivy/app/pomodoro.py
+++
@@ -1,6 +0,0 @@
-from kivymd.app import MDApp
-from kivy.lang import Builder
-
-class PomoDuno(MDApp):
-    def build(self):
-        return Builder.load_file('app/pomodoro.kv')
--- estudo kivy/main.py
+++
@@ -1,3 +0,0 @@
-from app.pomodoro import PomoDuno
-
-PomoDuno().run()
